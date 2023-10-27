9e58d0fb351ca06be1396b3313d5a9d71ed50c87
109310307 <146065460+109310307@users.noreply.github.com>
2023-10-27 19:35:12 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+
+//hw4-1
+int main()
+{
+int n;
+scanf("%d", &n);
+assert(1 <= n && n <= 50);
+int prime[n];
+
+for(int i = 0; i < n; i++)
+    scanf("%d", &prime[i]);
+
+for(int i = 0; i < n; i++)
+{
+    bool YN = true;
+    for(int j = 2; j < prime[i]; j++)
+    {
+        if (prime[i] % j == 0)
+            YN = false;
+    }
+
+    if (YN == true)
+        printf("YES ");
+    if (YN == false)
+        printf("NO ");
+}
+
+}
 
